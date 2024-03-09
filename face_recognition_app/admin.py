ae15c686d4e1b83c7cc8699336e9d4e0c04d300d
DavisWere <devisodhis10@gmail.com>
2024-03-09 07:29:24 +0300

    add model

--- face_recognition_app/admin.py
+++ face_recognition_app/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
-from .models import UserProfile, CapturedImage
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, UserProfile, CapturedImage
 
+
+admin.site.register(CustomUser)
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ['user', 'profile_picture']
 
-
-# @admin.register(CapturedImage)
-# class CapturedImageAdmin(admin.ModelAdmin):
-#     list_display = ['user', 'image']
+@admin.register(CapturedImage)
+class CapturedImageAdmin(admin.ModelAdmin):
+    list_display = ['user', 'image']
--- face_recognition_app/models.py
+++ face_recognition_app/models.py
@@ -1,12 +1,36 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
 
+
+class CustomUser(models.Model):
+    STUDENT = 'student'
+    STAFF = 'staff'
+    USER_TYPE_CHOICES = [
+        (STUDENT, 'student'),
+        (STAFF, 'staff'),
+    ]
+    email = models.EmailField(unique=True)
+    phone_code = models.CharField(max_length=4, blank=True, null=True, default="+254")
+    first_name = models.CharField(max_length=30, blank=True, null=True)
+    last_name = models.CharField(max_length=30, blank=True, null=True)
+    identification_number = models.CharField(max_length=50, unique=True)
+    avatar = models.FileField(blank=True, null=True)
+    user_type = models.CharField(max_length=10, choices=USER_TYPE_CHOICES, default = 'student')
+    username = models.CharField(max_length=128, unique=True, blank= True, null= True)
+    password = models.CharField(max_length=128, default='123456')
+    phone_number = models.CharField(max_length=10, blank=True, null=True, unique=True)
+    nationality = models.CharField(max_length=30, blank=True, null=True)
+    
+    def __str__(self):
+        return self.username
+    
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
 
 
 class CapturedImage(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='captured_images/')
