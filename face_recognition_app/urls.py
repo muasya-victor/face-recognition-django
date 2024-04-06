a533cb4852df2ff7bb9b92d821da349c32583599
DavisWere <devisodhis10@gmail.com>
2024-04-06 13:57:59 +0300

    Update face\_recognition\_app urls and views:
    - Remove commented-out import lines in urls.py
    - Add new paths for report and generate-report in urls.py
    - Remove compare\_images and profile paths from urls.py
    - Update views.py with report and generate\_pdf functions
    - Add necessary imports for pisa and reportlab in views.py
    - Update capture\_image function to accept GET requests
    - Comment out compare\_with\_user\_profiles and compare\_images functions in views.py
    - Add report and generate\_pdf functions in views.py
    - Update requirements.txt with new binary file

--- face_recognition_app/urls.py
+++ face_recognition_app/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-# from .views import capture_image, compare_images, login_view
+# from .views import capture_image compare_images, login_view
 from . import views 
 urlpatterns = [
-    path('capture/', views.capture_image, name='capture_image'),
+    path('', views.capture_image, name='capture_image'),
     path('login/', views.login_view, name='login'),
-    path('compare-images/', views.compare_images, name='compare_images'),
-    path('profile/', views.compare_with_user_profiles, name= 'compare_with_user_profiles')
+    # path('compare-images/', views.compare_images, name='compare_images'),
+    # path('profile/', views.compare_with_user_profiles, name= 'compare_with_user_profiles'),
+    path('report/', views.report, name= 'Admin  Report Page'),
+    path('generate-report', views.generate_pdf, name ="Generate PDF"),
+
 ]
--- face_recognition_app/views.py
+++ face_recognition_app/views.py
@@ -3,13 +3,19 @@ from django.http import JsonResponse
 from .models import UserProfile , CustomUser
 import base64
 import numpy as np
-import cv2
+from xhtml2pdf import pisa 
+# from weasyprint import HTML
+import cv2 
+from reportlab.pdfgen import canvas
+from reportlab.lib import colors
 from django.shortcuts import render, redirect
 from django.shortcuts import get_object_or_404
+from django.template.loader import get_template
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
+from django.http import HttpResponse
 
 @login_required
 def capture_image(request):
@@ -31,95 +37,54 @@ def capture_image(request):
     }
 
     return render(request, 'capture_image.html', context)
-def compare_images(request):
-    if request.method == 'POST' and 'user_id' in request.POST:
-        user_id = request.POST['user_id']
+# def compare_images(request):
+#     if request.method == 'POST' and 'user_id' in request.POST:
+#         user_id = request.POST['user_id']
         
-        # Get UserProfile object based on user_id
-        user_profile = UserProfile.objects.filter(user_id=user_id).first()
+#         # Get UserProfile object based on user_id
+#         user_profile = UserProfile.objects.filter(user_id=user_id).first()
 
-        if user_profile and user_profile.profile_picture:
-            # Read the image data from the profile_picture field
-            image_data = user_profile.profile_picture.read()
+#         if user_profile and user_profile.profile_picture:
+#             # Read the image data from the profile_picture field
+#             image_data = user_profile.profile_picture.read()
 
-            # Convert base64 image data to NumPy array
-            try:
-                image_np_array = np.frombuffer(image_data, dtype=np.uint8)
+#             # Convert base64 image data to NumPy array
+#             try:
+#                 image_np_array = np.frombuffer(image_data, dtype=np.uint8)
 
-                # Decode the image array to OpenCV format
-                image = cv2.imdecode(image_np_array, cv2.IMREAD_COLOR)
+#                 # Decode the image array to OpenCV format
+#                 image = cv2.imdecode(image_np_array, cv2.IMREAD_COLOR)
 
-                # Convert the input image to RGB (assuming it's in BGR format)
-                image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+#                 # Convert the input image to RGB (assuming it's in BGR format)
+#                 image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-                # Perform face recognition or other processing
-                identified_user = compare_with_user_profiles(image_rgb)
+#                 # Perform face recognition or other processing
+#                 identified_user = compare_with_user_profiles(image_rgb)
 
-                if identified_user:
-                    user_details = {
-                        'username': identified_user.username,
-                        'email': identified_user.email,
-                        'first_name': identified_user.first_name,
-                        'last_name': identified_user.last_name,
-                        # Add more user details as needed
-                    }
-                    print(user_details)
-                    return JsonResponse({'message': 'User found', 'user_details': user_details})
-                else:
-                    return JsonResponse({'message': 'User not found'})
+#                 if identified_user:
+#                     user_details = {
+#                         'username': identified_user.username,
+#                         'email': identified_user.email,
+#                         'first_name': identified_user.first_name,
+#                         'last_name': identified_user.last_name,
+#                         # Add more user details as needed
+#                     }
+#                     print(user_details)
+#                     return JsonResponse({'message': 'User found', 'user_details': user_details})
+#                 else:
+#                     return JsonResponse({'message': 'User not found'})
 
-            except Exception as e:
-                return JsonResponse({'error': f'Error processing image: {e}'}, status=400)
+#             except Exception as e:
+#                 return JsonResponse({'error': f'Error processing image: {e}'}, status=400)
 
-        else:
-            return JsonResponse({'message': 'User profile not found or profile picture is empty'}, status=400)
+#         else:
+#             return JsonResponse({'message': 'User profile not found or profile picture is empty'}, status=400)
 
-    else:
-        return JsonResponse({'error': 'Invalid request'}, status=400)
+#     else:
+#         return JsonResponse({'error': 'Invalid request'}, status=400)
 
-def compare_with_user_profiles(request):
-    # env = Env()
-    # env.read_env()  # Load environment variables from .env file
 
-    CustomUser = get_user_model()
 
-    if request.method == 'POST' and 'user_id' in request.POST:
-        user_id = request.POST['user_id']
-        
-        # Get UserProfile object based on user_id
-        user_profile = get_object_or_404(UserProfile, user_id=1)
-
-        # Check if the profile_picture field is not empty
-        if user_profile.profile_picture:
-            # Read the image data from the profile_picture field
-            image_data = user_profile.profile_picture.read()
-
-            # Convert base64 image data to NumPy array
-            try:
-                image_np_array = np.frombuffer(image_data, dtype=np.uint8)
-
-                # Decode the image array to OpenCV format
-                image = cv2.imdecode(image_np_array, cv2.IMREAD_COLOR)
-
-                # Convert the input image to grayscale
-                gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-                # Process the image (e.g., face detection and recognition)
-                # ...
-                
-                # Example response for demonstration
-                response_data = {'message': 'Image processed successfully'}
-                return JsonResponse(response_data)
-
-            except Exception as e:
-                error_message = f"Error processing image: {e}"
-                return JsonResponse({'error': error_message}, status=400)
-        
-        else:
-            return JsonResponse({'error': 'Profile picture not found or empty'}, status=400)
-
-    else:
-        return JsonResponse({'error': 'Invalid request method or missing user_id'}, status=400)
 def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
@@ -135,3 +100,42 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('login')  # Redirect to the login page after logout
+
+
+@login_required
+
+def report(request):
+    user = request.user
+    users = CustomUser.objects.all()
+    context  = {"users": users}
+    if user is not None:
+        if user.is_superuser:
+            return render(request, 'admin.html', context)
+        else:
+            return redirect('compare-images/')
+        
+    else:
+        return redirect('login/')
+
+
+def generate_pdf(request):
+    # Retrieve data for the PDF from the database or any other source
+    users = CustomUser.objects.all()
+    context = {"users": users}
+
+    # Render template
+    template = get_template('admin.html')
+    html = template.render(context)
+
+    # Create PDF file
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="admin_report.pdf"'
+
+    # Generate PDF from HTML content
+    pisa_status = pisa.CreatePDF(html, dest=response)
+
+    # If PDF generation failed, return an error message
+    if pisa_status.err:
+        return HttpResponse('PDF generation error!')
+
+    return response
